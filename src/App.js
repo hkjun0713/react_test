35160ea039020b20c69928895daee6fab8a98192
k jun <hkjun0713@naver.com>
2023-08-04 09:41:42 +0900

    aaa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React aaa
+          Learn React
         </a>
       </header>
     </div>
